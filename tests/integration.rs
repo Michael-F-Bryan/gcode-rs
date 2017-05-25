8211b176f51871897fb4adccf8bf3be563568b54
Michael-F-Bryan <michaelfbryan@gmail.com>
2017-05-25 15:15:29 +0800

    Added Parser to our integration tests

--- tests/integration.rs
+++ tests/integration.rs
@@ -4,6 +4,7 @@
 extern crate gcode;
 
 use gcode::lexer::Tokenizer;
+use gcode::parser::Parser;
 use gcode::Result;
 
 const PROGRAMS: [&'static str; 3] = [include_str!("data/program_1.gcode"),
@@ -11,8 +12,22 @@ const PROGRAMS: [&'static str; 3] = [include_str!("data/program_1.gcode"),
                                      include_str!("data/program_3.gcode")];
 
 #[test]
+fn parse_all_programs() {
+    for (i, program) in PROGRAMS.iter().enumerate() {
+        println!("Program {}", i + 1);
+
+        let tokenizer = Tokenizer::new(program.chars());
+        let tokens = tokenizer.filter_map(|t| t.ok());
+
+        let parsed_commands: Result<Vec<_>> = Parser::new(tokens).collect();
+        parsed_commands.unwrap();
+    }
+}
+
+#[test]
 fn lex_all_programs() {
-    for program in &PROGRAMS {
+    for (i, program) in PROGRAMS.iter().enumerate() {
+        println!("Program {}", i + 1);
         let tokenizer = Tokenizer::new(program.chars());
         let tokens: Result<Vec<_>> = tokenizer.collect();
 
