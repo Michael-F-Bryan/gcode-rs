18a548be1a005f9e48f11073dc3dc5ff437a430d
Michael Bryan <michaelfbryan@gmail.com>
2018-07-11 20:08:39 +0800

    Finished writing the basic usage example

--- examples/basic-usage.rs
+++ examples/basic-usage.rs
@@ -8,7 +8,7 @@ fn main() {
     let mut g_codes = 0;
     let mut m_codes = 0;
     let mut total_args = 0;
-    let mut x_arguments = 0;
+    let mut cumulative_x = 0.0;
 
     for block in gcode::parse(PROGRAM_1) {
         println!("{:#?}", block);
@@ -21,10 +21,13 @@ fn main() {
 
         total_args += block.args().len();
 
-        if let Some(
+        if let Some(x) = block.value_for('X') {
+            cumulative_x += x;
+        }
     }
 
     println!("G-codes: {}", g_codes);
     println!("M-codes: {}", m_codes);
     println!("Total Arguments: {}", total_args);
+    println!("Total Displacement in the X direction: {}", cumulative_x);
 }
