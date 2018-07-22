3df5c79c64c5bb6be3539a8e206774dfb2897c3a
Michael Bryan <michaelfbryan@gmail.com>
2018-07-22 15:52:17 +0800

    Switched back to using trust for CI

---
+++ ci/before_deploy.sh
@@ -0,0 +1,46 @@
+# This script takes care of building your crate and packaging it for release
+
+set -ex
+
+generate_docs() {
+    cargo doc --all --verbose
+    echo '<head><meta http-equiv="refresh" content="0; URL=gcode/index.html" /></head>' > target/doc/index.html
+}
+
+generate_bundle() {
+    test -f Cargo.lock || cargo generate-lockfile
+
+    cross rustc --lib --target $TARGET --release -- -C lto
+    cp target/$TARGET/release/libgcode.{so,a} $stage/
+
+    # We need the cbindgen tool for generating our header file
+    if [ -f $HOME/.cargo/env ]; then source $HOME/.cargo/env; fi
+    command -v cbindgen >/dev/null 2>&1 || cargo install --debug cbindgen
+
+    cbindgen --output $stage/gcode.h
+
+    cd $stage
+    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+    cd $src
+
+    rm -rf $stage
+}
+
+main() {
+    local src=$(pwd) \
+          stage=
+
+    case $TRAVIS_OS_NAME in
+        linux)
+            stage=$(mktemp -d)
+            ;;
+        osx)
+            stage=$(mktemp -d -t tmp)
+            ;;
+    esac
+
+    generate_docs
+    generate_bundle
+}
+
+main
