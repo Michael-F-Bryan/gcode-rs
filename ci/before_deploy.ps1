55c0136868df3087e4d959a970c7d48620a32198
Michael Bryan <michaelfbryan@gmail.com>
2018-09-19 19:15:28 +0800

    Tweaked the windows builds [skip ci]

--- ci/before_deploy.ps1
+++ ci/before_deploy.ps1
@@ -9,9 +9,12 @@ New-Item -Type Directory -Name $STAGE
 Set-Location $STAGE
 
 $ZIP = "$SRC_DIR\$($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME)-$($Env:TARGET).zip"
+$RELEASE_DIR = "$SRC_DIR\target\$($Env:TARGET)\release"
 
-Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\libgcode.dll" '.\'
-Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\libgcode.a" '.\'
+Get-ChildItem $RELEASE_DIR
+Copy-Item "$RELEASE_DIR\gcode.dll" '.\'
+Copy-Item "$RELEASE_DIR\gcode.dll.lib" '.\'
+Copy-Item "$RELEASE_DIR\gcode.lib" '.\'
 
 7z a "$ZIP" *
 
