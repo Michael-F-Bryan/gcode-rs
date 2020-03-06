e22a806041b1fa35f96bfd3daa1e3d633c7a0e92
Michael-F-Bryan <michaelfbryan@gmail.com>
2020-03-06 23:45:23 +0800

    Manually implemented Debug for Line

--- gcode/src/line.rs
+++ gcode/src/line.rs
@@ -1,10 +1,11 @@
 use crate::{
-    buffers::{Buffer, Buffers, CapacityError, DefaultBuffers},
+    buffers::{self, Buffer, Buffers, CapacityError, DefaultBuffers},
     Comment, GCode, Span, Word,
 };
+use core::fmt::{self, Debug, Formatter};
 
 /// A single line, possibly containing some [`Comment`]s or [`GCode`]s.
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Clone, PartialEq)]
 #[cfg_attr(
     feature = "serde-1",
     derive(serde_derive::Serialize, serde_derive::Deserialize)
@@ -14,6 +15,28 @@ pub struct Line<'input, B: Buffers<'input> = DefaultBuffers> {
     comments: B::Comments,
     line_number: Option<Word>,
     span: Span,
+}
+
+impl<'input, B> Debug for Line<'input, B>
+where
+    B: Buffers<'input>,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        // explicitly implement Debug because the normal derive is too strict
+        let Line {
+            gcodes,
+            comments,
+            line_number,
+            span,
+        } = self;
+
+        f.debug_struct("Line")
+            .field("gcodes", &buffers::debug(gcodes))
+            .field("comments", &buffers::debug(comments))
+            .field("line_number", line_number)
+            .field("span", span)
+            .finish()
+    }
 }
 
 impl<'input, B> Default for Line<'input, B>
