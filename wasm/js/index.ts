77678159bea18080d05afdee14c44c4ac6eda3eb
Michael-F-Bryan <michaelfbryan@gmail.com>
2020-03-08 04:09:00 +0800

    Put together most of the webpack scaffolding

---
+++ wasm/js/index.ts
@@ -0,0 +1,3 @@
+import * as wasm from "../pkg/index";
+
+wasm.greet("World");
---
+++ wasm/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
+module.exports = {
+    entry: './js/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new WasmPackPlugin({
+            crateDirectory: __dirname,
+        }),
+    ]
+};
